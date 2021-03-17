7eb3a7f0892712d5549d00afad5ec27b12978d62
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-18 01:18:36 +0530

    Tested : Wrote Junit test check no. of contacts were added in particular period

--- test/java/com/javapractice/AddressBookServiceTest.java
+++ test/java/com/javapractice/AddressBookServiceTest.java
@@ -5,6 +5,7 @@ import com.javapractise.AddressBookService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class AddressBookServiceTest {
@@ -23,5 +24,19 @@ public class AddressBookServiceTest {
         addressBookService.updatePersonCity("ganesh","bangalore");
         boolean result = addressBookService.checkAddressBookDataInSyncWithDB("ganesh");
         Assertions.assertTrue(result);
+
+    }
+
+    @Test
+    public void givenDateRange_WhenRetrieved_ShouldMatchAddressBookContactCount() {
+        AddressBookService addressBookService = new AddressBookService();
+        addressBookService.readAddressBookDataFromDB(AddressBookService.IOService.DB_IO);
+        LocalDate startDate = LocalDate.of(2018,01,01);
+        LocalDate endDate = LocalDate.now();
+        List<AddressBookData>  addressBookDataList =
+                addressBookService.readEmployeePayrollDataForDateRange(AddressBookService.IOService.DB_IO,startDate,endDate);
+        System.out.println(addressBookDataList);
+        Assertions.assertEquals(4,addressBookDataList.size());
+
     }
 }
